3935f0651c60f0e0878e07db5d1931649ac436c6
JuanDavidPardo <juan23.pa@hotmil.com>
2017-04-01 00:36:42 -0500

    Comentar -  reducir el codigo
    Reducimos el código utilizando un if corto y evitando if anidados y
    códigos repetidos, tambien comentaríamos el código para mejor
    entendimiento.

--- src/PuntoPalíndromos.java
+++ src/PuntoPalíndromos.java
@@ -2,52 +2,62 @@ import java.awt.*;
 import java.io.*;
 
 /**
- * Created by JUANDAVID on 30/03/17.
+ * Clase que muetra el proximo numero palindromo
+ * @author
  */
 public class PuntoPalíndromos {
 
+    /**
+     * Metodo que busca el proximo palindromo y lo escribe en un archivo
+     * @variable <bf> bufer nos per va permitir acceder a un archivo
+     * @variable <numero> representa los numero que contiene el archivo
+     * @variable <cadena> representa los numeros que contiene el archivo en forma de String
+     * @variable <tamano> es el tamaño (cantidad de caracteres) del numero
+     * @variable <polindromo> controla si se econtro o no el numero (verdadero o falso)
+     * @variables <mitadA, mitadB> son las mitades de la cadena a comparar
+     */
     public static void leerArchivo() throws IOException {
 
         BufferedReader bf = new BufferedReader(new FileReader("./entrada.in"));
-        int numero = 0;
-        String cadena = "";
-        int tamano = 0;
+        int numero = 0, tamano = 0;
+        String cadena = "", mitadA, mitadB;
         boolean polindorno = false;
 
+        // Recorremos todas las lineas del archivo
         while ((cadena = bf.readLine()) != null) {
-            polindorno = false;
+
+            polindorno = false;// en cada linea permitimos la busqueda por lo menos una vez
+
             while(!polindorno){
+
                 tamano = cadena.length();
-                if (tamano % 2 == 0 ) {
 
-                    if(cadena.substring(0,(tamano/2)).equals(new StringBuilder(cadena.substring((tamano/2), tamano)).reverse().toString())){
-                        polindorno = true;
-                        System.out.println(cadena);
-                    }else{
-                       numero = Integer.parseInt(cadena);
-                       numero++;
-                       cadena = Integer.toString(numero);
-                        System.out.println(cadena.substring(0,(tamano/2)));
-                        System.out.println(new StringBuilder(cadena.substring((tamano/2), tamano)).reverse());
+                // Si el tamaño del numero es par los partimos a la mitad sino sumanos 1 a la mitad para no tomar
+                // el numero situado en la mitad del mismo
+                mitadA = cadena.substring(0,(tamano/2));
+                mitadB = (tamano % 2 == 0 )?
+                           new StringBuilder(cadena.substring((tamano/2), tamano)).reverse().toString()
+                         : new StringBuilder(cadena.substring((tamano/2)+1, tamano)).reverse().toString();
 
-                    }
+                //Dividimos la cadena en dos Strings y la conparamos pero la segun parte la reversamos ej : abc ->n cba
+                // si son iguales encontramos el numero
+                if(mitadA.equals(mitadB)){
+                    polindorno = true;
+                    System.out.println(cadena);
                 }else{
-                    if(cadena.substring(0,(tamano/2)).equals(new StringBuilder(cadena.substring((tamano/2)+1, tamano)).reverse().toString())){
-                        polindorno = true;
-                        System.out.println(cadena);
-                    }else{
-                        numero = Integer.parseInt(cadena);
-                        numero++;
-                        cadena = Integer.toString(numero);
-                        System.out.println(cadena.substring(0,(tamano/2)));
-                        System.out.println(new StringBuilder(cadena.substring((tamano/2)+1, tamano)).reverse());
-                    }
+
+                   // convertimos la cadena a numero para poderle aumentar de a 1
+                   numero = Integer.parseInt(cadena);
+                   numero++;
+                   cadena = Integer.toString(numero);
+                    System.out.println(cadena.substring(0,(tamano/2)));
+                    System.out.println(new StringBuilder(cadena.substring((tamano/2), tamano)).reverse());
+
                 }
-
             }
-            
-        }
 
+        }
+        
     }
 
 
